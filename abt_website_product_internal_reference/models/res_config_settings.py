fe0029e800e750e35ff2d46409bc139f596b6927
arpitn2912 <arpitmet2912@gmail.com>
2024-05-04 09:47:51 +0530

    Website Product Internal Reference V17

---
+++ abt_website_product_internal_reference/models/res_config_settings.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+from odoo import api, fields, models
+
+class ResConfigSettings(models.TransientModel):
+    _inherit = 'res.config.settings'
+
+    is_shown_product_int_ref = fields.Boolean("Is Shown Product Internal Reference?",config_parameter='abt_website_product_internal_reference.is_shown_product_int_ref')
+    internal_reference_font_size = fields.Char("Font Size",config_parameter='abt_website_product_internal_reference.internal_reference_font_size')
+    internal_reference_font_color = fields.Char("Font Color",config_parameter='abt_website_product_internal_reference.internal_reference_font_color')
+    is_internal_reference_font_bold = fields.Boolean("Is Font Bold?",config_parameter='abt_website_product_internal_reference.is_internal_reference_font_bold')
---
+++ abt_website_product_internal_reference/static/src/js/internal_reference.js
@@ -0,0 +1,24 @@
+/** @odoo-module **/
+
+import { jsonrpc } from "@web/core/network/rpc_service";
+    $(document).ready(function(){
+        $('.product_id').change(function(){
+            var productId = document.querySelector('.product_id').value;
+            jsonrpc('/get_product_id',{
+                'product_id' : productId
+            }).then(function(data){
+               document.getElementById('product_internal_reference').innerText = data.default_code
+                if(data.product_internal_reference == "True"){
+                    if(data.default_code != " "){
+                        $('.box').show();
+                    }
+                    else{ 
+                        $('.box').hide();
+                    }
+                }
+                else{
+                    $('.box').hide();
+                }
+            }); 
+         });
+    });
