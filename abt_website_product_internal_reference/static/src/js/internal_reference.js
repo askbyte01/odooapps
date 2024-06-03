0ec6b80eee317237fba08e8716d0dbc2f6aead22
askbyte01 <askbytetechnolab@gmail.com>
2024-06-03 21:27:09 +0530

    [ADD] abt_website_product_internal_reference

--- abt_website_product_internal_reference/static/src/js/internal_reference.js
+++ abt_website_product_internal_reference/static/src/js/internal_reference.js
@@ -1,29 +1,25 @@
 odoo.define('abt_website_product_internal_reference.internal_reference', function (require) {
-'use strict';
+    'use strict';
     var ajax = require('web.ajax');
+
     $(document).ready(function(){
         $('.product_id').change(function(){
-            var productId = document.querySelector('.product_id').value;
-            ajax.jsonRpc('/get_product_id', "call",{
-                'product_id' : productId
+            var productId = document.querySelector('.product_id')?.value;
+            ajax.jsonRpc('/get_product_id', "call", {
+                'product_id': productId
             }).then(function(data){
-               document.getElementById('product_internal_reference').innerText = data.default_code
-                if(data.product_internal_reference == "True")
-                {
-                    if(data.default_code != " ")
-                    {
+                var productInternalReferenceElem = document.getElementById('product_internal_reference');
+                if (productInternalReferenceElem) {
+                    productInternalReferenceElem.innerText = data.default_code;
+
+                    if (data.product_internal_reference === "True" && data.default_code.trim() !== "") {
                         $('.box').show();
-                    }
-                    else
-                    { 
+                    } else {
                         $('.box').hide();
                     }
                 }
-                else
-                {
-                    $('.box').hide();
-                } 
-            }); 
-         });
+            });
+        });
     });
 });
+
