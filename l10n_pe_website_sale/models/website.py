6b13828428cdb4762fe2c0efafd06b9074f47e11
askbyte01 <askbytetechnolab@gmail.com>
2024-06-21 22:58:12 +0530

    [ADD] l10n_pe_website_sale_v16

---
+++ l10n_pe_website_sale/models/website.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+from odoo import model
+
+class Website(models.Model):
+    _inherit = "website"
+
+    def _display_partner_b2b_fields(self):
+        """Peruvian localization must always display b2b fields"""
+        self.ensure_one()
+        return self.company_id.country_id.code == "PE" or super()._display_partner_b2b_fields()
