47dcce593b43122be33c1029d1b9e2a6c4dd3c1e
askbyte01 <askbytetechnolab@gmail.com>
2024-06-22 21:59:28 +0530

    [ADD] abt_separate_customer_invoice_v15

---
+++ abt_separate_customer_invoice/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    "name"        : "Separate Customer Invoice",
+    "author"      : "AskByte Technolab",
+    "category"    : "Sales/Sales",
+    "summary"     : """Separate Customer Invoice""",
+    "description" : """ """,
+    "version"     : "15.0.1.0",
+    "depends"     : ["sale_management","stock"],
+    "data"        : [
+        'wizard/sale_make_invoice_advance_views.xml',
+    ],
+    "license"     : "LGPL-3",
+    "installable" : True,
+    "application" : True,
+    "auto_install": False,
+}
