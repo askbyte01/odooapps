189cc16e7fd9b087f229dffc6b018e8c89b979c8
askbyte01 <askbytetechnolab@gmail.com>
2024-08-15 15:15:05 +0530

    [ADD] abt_pos_total_item_qty_display_v16

--- abt_pos_total_item_qty_display/__manifest__.py
+++ abt_pos_total_item_qty_display/__manifest__.py
@@ -5,7 +5,7 @@
     "author": "AskByte Technolab",
     "category": "Point of Sale",
     "summary": "POS Total Quantity & Items Display",
-    "license": "OPL-1",
+    "license": "AGPL-3",
     "description": """
         Total Items and Total Quantity are displayed in the POS module 
         based on the configuration settings for showing these details.
